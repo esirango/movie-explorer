52ab52958f02b34e6e19ed3bb61ee66738fef19f
esirango <esirango23@gmail.com>
2025-07-04 19:10:07 +0330

    fix rtl ltr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
+import { useEffect } from "react";
 import { ThemeProvider } from "../components/theme/ThemeProvider";
-import { LanguageProvider } from "../contexts/lang/LanguageContext";
+import {
+    LanguageProvider,
+    useLanguage,
+} from "../contexts/lang/LanguageContext";
 import "../styles/globals.css";
 
 import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }) {
+    function DirectionSetter() {
+        const { language } = useLanguage();
+
+        useEffect(() => {
+            document.documentElement.dir = language === "fa" ? "rtl" : "ltr";
+        }, [language]);
+
+        return null;
+    }
     return (
         <LanguageProvider>
+            <DirectionSetter />
             <ThemeProvider>
                 <NextNProgress
                     color="#6366f1"

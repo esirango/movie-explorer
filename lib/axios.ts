64af02f4700934c95b58580339936c24fd2d0ba8
esirango <esirango23@gmail.com>
2025-07-04 18:48:36 +0330

    add base url to env local file

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
+const TMDB_BASE_API_URL = process.env.TMDB_BASE_API_URL;
 
 const tmdb = axios.create({
-    baseURL: "https://api.themoviedb.org/3",
+    baseURL: TMDB_BASE_API_URL,
     params: {
         api_key: TMDB_API_KEY,
         language: "en-US",
--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     env: {
         TMDB_API_KEY: process.env.TMDB_API_KEY,
+        TMDB_BASE_API_URL: process.env.TMDB_BASE_API_URL,
     },
 };
 

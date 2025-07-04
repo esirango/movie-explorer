22c9144393d81ffb517481105d5dd49f03edc458
esirango <esirango23@gmail.com>
2025-07-04 17:52:43 +0330

    add next progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import { ThemeProvider } from "../components/theme/ThemeProvider";
 import "../styles/globals.css";
+
+import NextNProgress from "nextjs-progressbar";
+
 export default function App({ Component, pageProps }) {
     return (
         <ThemeProvider>
+            <NextNProgress
+                color="#6366f1"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={3}
+                showOnShallow={true}
+            />
             <Component {...pageProps} />
         </ThemeProvider>
     );

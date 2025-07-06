e63ec7bbaff9526fcf81eda218761cf22bead39c
esirango <esirango23@gmail.com>
2025-07-06 19:30:44 +0330

    add more genres

--- components/movies/MovieFilter.tsx
+++ components/movies/MovieFilter.tsx
@@ -36,9 +36,24 @@ const MovieFilter: React.FC<MovieFilterProps> = ({
     const genres = [
         { id: "", name: t("movies.filters.genre"), disabled: true },
         { id: "28", name: t("movies.filters.genres.28") },
+        { id: "12", name: t("movies.filters.genres.12") },
+        { id: "16", name: t("movies.filters.genres.16") },
         { id: "35", name: t("movies.filters.genres.35") },
+        { id: "80", name: t("movies.filters.genres.80") },
+        { id: "99", name: t("movies.filters.genres.99") },
         { id: "18", name: t("movies.filters.genres.18") },
+        { id: "10751", name: t("movies.filters.genres.10751") },
+        { id: "14", name: t("movies.filters.genres.14") },
+        { id: "36", name: t("movies.filters.genres.36") },
         { id: "27", name: t("movies.filters.genres.27") },
+        { id: "10402", name: t("movies.filters.genres.10402") },
+        { id: "9648", name: t("movies.filters.genres.9648") },
+        { id: "10749", name: t("movies.filters.genres.10749") },
+        { id: "878", name: t("movies.filters.genres.878") },
+        { id: "10770", name: t("movies.filters.genres.10770") },
+        { id: "53", name: t("movies.filters.genres.53") },
+        { id: "10752", name: t("movies.filters.genres.10752") },
+        { id: "37", name: t("movies.filters.genres.37") },
     ];
 
     const countries = [

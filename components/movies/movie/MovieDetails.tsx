c4914f0241e558c12d948bf7e9e015622f93e2db
esirango <esirango23@gmail.com>
2025-07-10 15:26:23 +0330

    remove extra console.log

--- components/movies/movie/MovieDetails.tsx
+++ components/movies/movie/MovieDetails.tsx
@@ -11,7 +11,6 @@ function MovieDetails({ movie, related }) {
 
     const [posterError, setPosterError] = useState<boolean>(false);
 
-    console.log(movie);
     return (
         <div className="container mx-auto px-4 py-8">
             <div className="flex flex-col md:flex-row gap-8">

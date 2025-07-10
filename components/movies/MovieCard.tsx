00b03e54a2522be1825751e9c4892b7f9eacea1f
esirango <esirango23@gmail.com>
2025-07-10 15:13:10 +0330

    same height for movies with no overview

--- components/movies/MovieCard.tsx
+++ components/movies/MovieCard.tsx
@@ -11,6 +11,11 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
     const [imageError, setImageError] = useState(false);
 
+    function truncateText(text: string, maxLength: number) {
+        if (text.length <= maxLength) return text;
+        return text.slice(0, maxLength) + "...";
+    }
+
     return (
         <Link href={`/movies/${movie.id}/${movie.title}`}>
             <div className="bg-white dark:bg-gray-800 rounded-xl overflow-hidden shadow-md hover:shadow-xl transition-all duration-200 transform hover:scale-105 cursor-pointer">
@@ -26,16 +31,16 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
                         <div className="text-5xl mb-2">🎬</div>
                     </div>
                 )}
-                <div className="p-4 flex flex-col gap-2">
+                <div className="p-4 flex flex-col h-[140px]">
                     <h3 className="text-lg font-semibold truncate text-gray-800 dark:text-white">
                         {movie.title}
                     </h3>
 
-                    <p className="text-sm text-gray-700 dark:text-gray-300 line-clamp-3">
-                        {movie.overview}
+                    <p className="text-sm text-gray-700 dark:text-gray-300 overflow-hidden flex-grow">
+                        {truncateText(movie.overview, 70)}
                     </p>
 
-                    <div className="flex items-center justify-between text-xs text-gray-500 dark:text-gray-400 mt-2">
+                    <div className="flex items-center justify-between text-xs text-gray-500 dark:text-gray-400 pt-4">
                         <MovieReleaseDate
                             releaseDate={movie.release_date}
                             type="list"

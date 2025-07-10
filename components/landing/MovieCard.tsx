e070d6351aba904bef5af2a25ed0f2dae8b668af
esirango <esirango23@gmail.com>
2025-07-10 12:26:16 +0330

    better ux for show imgs with error in landing

--- components/landing/MovieCard.tsx
+++ components/landing/MovieCard.tsx
@@ -19,6 +19,7 @@ function MovieCard({
     });
 
     const [visible, setVisible] = useState(false);
+    const [imgError, setImgError] = useState(false);
 
     useEffect(() => {
         if (inView) {
@@ -32,27 +33,30 @@ function MovieCard({
             <div
                 ref={ref}
                 className={`lg:w-[500px] md:w-[300px] min-w-[200px] rounded-lg overflow-hidden shadow-lg
-                bg-white text-gray-900
-                dark:bg-gray-800 dark:text-gray-100
-                mx-3 my-5 hover:scale-105 cursor-pointer
-                transform transition-all duration-700 ease-out
-                ${
-                    visible
-                        ? "opacity-100 translate-y-0"
-                        : "opacity-0 translate-y-6"
-                }
-            `}
+      bg-white text-gray-900
+      dark:bg-gray-800 dark:text-gray-100
+      mx-3 my-5 hover:scale-105 cursor-pointer
+      transform transition-all duration-700 ease-out
+      ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-6"}
+    `}
             >
                 {visible ? (
-                    <img
-                        src={poster}
-                        alt={title}
-                        className="w-full h-fit object-cover"
-                        loading="lazy"
-                        draggable={false}
-                    />
+                    !imgError ? (
+                        <img
+                            src={poster}
+                            alt={title}
+                            className="w-full aspect-[2/3] min-h-[300px] md:min-h-auto object-cover"
+                            loading="lazy"
+                            draggable={false}
+                            onError={() => setImgError(true)}
+                        />
+                    ) : (
+                        <div className="w-full aspect-[2/3] min-h-[300px] md:min-h-auto flex items-center justify-center bg-gray-300 dark:bg-gray-700">
+                            <span className="text-5xl">🎬</span>
+                        </div>
+                    )
                 ) : (
-                    <div className="w-full h-64 bg-gray-200 dark:bg-gray-700 animate-pulse" />
+                    <div className="w-full aspect-[2/3] min-h-[300px] md:min-h-auto bg-gray-200 dark:bg-gray-700 animate-pulse" />
                 )}
                 <div className="p-4">
                     <h3 className="font-semibold text-lg">{title}</h3>

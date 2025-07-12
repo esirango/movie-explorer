0c67708fa0ae5ea8e83f08c7c6cb27e8b0ab8b02
esirango <esirango23@gmail.com>
2025-07-13 01:51:41 +0330

    add layout for auth pages

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import LoginPage from "../../../components/auth/LoginForm";
+import Navbar from "../../../components/layout/Navbar";
+import Footer from "../../../components/layout/Footer";
 
 function index() {
     return (
         <>
+            <Navbar />
             <LoginPage />
+            <Footer />
         </>
     );
 }
--- pages/auth/register/index.tsx
+++ pages/auth/register/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import RegisterForm from "../../../components/auth/RegisterationForm";
+import Navbar from "../../../components/layout/Navbar";
+import Footer from "../../../components/layout/Footer";
 
 function index() {
     return (
         <>
+            <Navbar />
             <RegisterForm />
+            <Footer />
         </>
     );
 }

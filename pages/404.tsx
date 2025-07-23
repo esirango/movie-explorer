cd09f515c0016d49967a7427635832c896fd1157
esirango <esirango23@gmail.com>
2025-07-23 21:15:33 +0330

    redesign 404 & 500 error page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useRouter } from "next/router";
 import SpinningFilmReel from "../components/error/SpinnerFilmReel";
+import { useLanguage } from "../lang/LanguageContext";
 
 export default function Custom404() {
     const router = useRouter();
+    const { t } = useLanguage();
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-gray-100 px-4 text-center">
@@ -11,16 +13,17 @@ export default function Custom404() {
                 4
                 <SpinningFilmReel />4
             </h1>
-            <h2 className="text-3xl font-semibold mb-4">Page Not Found</h2>
-            <p className="max-w-md mb-8 text-gray-400">
-                Sorry, the page you are looking for does not exist. <br />
-                The link might be broken or the page has been removed.
+            <h2 className="text-3xl font-semibold mb-4">
+                {t("errorPage.pageNotFound")}
+            </h2>
+            <p className="max-w-md mb-8 text-gray-400 whitespace-pre-line">
+                {t("errorPage.pageNotFoundDesc")}
             </p>
             <button
                 onClick={() => router.push("/")}
                 className="px-6 py-3 bg-indigo-600 hover:bg-indigo-700 rounded-full text-white font-semibold transition"
             >
-                Go Back Home
+                {t("errorPage.backHome")}
             </button>
         </div>
     );
--- pages/_error.tsx
+++ pages/_error.tsx
@@ -4,11 +4,18 @@ import { useLanguage } from "../lang/LanguageContext";
 import SpinningFilmReel from "../components/error/SpinnerFilmReel";
 
 const ErrorPage = ({ statusCode }: { statusCode?: number }) => {
-    const { t } = useLanguage();
+    const { t, language } = useLanguage();
 
     return (
         <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 dark:bg-gray-900 text-center px-4">
-            <SpinningFilmReel />
+            <h1
+                className={`text-[11rem] text-[#C7D2FE] font-extrabold mb-6 select-none flex gap-4 items-center ${
+                    language === "fa" ? "flex-row-reverse" : "flex"
+                }`}
+            >
+                5
+                <SpinningFilmReel /> <SpinningFilmReel />
+            </h1>
 
             <h1 className="text-4xl font-extrabold text-gray-800 dark:text-white mb-2">
                 {statusCode === 404
